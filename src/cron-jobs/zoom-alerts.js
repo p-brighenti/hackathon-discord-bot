f0370a2573b027cba1f2158f230ae06844dbda7b
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-24 20:57:53 +0100

    silenced zoom alerts before hackathon starts

--- src/cron-jobs/zoom-alerts.js
+++ src/cron-jobs/zoom-alerts.js
@@ -2,20 +2,29 @@ const cron = require('node-cron');
 const CHANNELS = require('../enums/channels');
 const MESSAGES = require('../enums/messages');
 const scheduleConfig = require('../config/schedule');
+const startDate = new Date('2020-04-28T09:00:00');
 
 exports.setup = (client) => {
     const channel = client.channels.cache.get(CHANNELS.GENERAL);
 
     const alert30Before = cron.schedule(
         scheduleConfig.ZOOM_ALERT_30,
-        async () => await channel.send(MESSAGES.THIRTY_MIN)
+        async () => {
+            if (hasHackathonStarted()) await channel.send(MESSAGES.THIRTY_MIN);
+        }
     );
 
     const alert10Before = cron.schedule(
         scheduleConfig.ZOOM_ALERT_10,
-        async () => await channel.send(MESSAGES.TEN_MIN)
+        async () => {
+            if (hasHackathonStarted()) await channel.send(MESSAGES.TEN_MIN);
+        }
     );
 
     alert30Before.start();
     alert10Before.start();
 };
+
+function hasHackathonStarted() {
+    return new Date() > startDate;
+}
