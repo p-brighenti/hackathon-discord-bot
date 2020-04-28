15a25de8c88dc36d03f72d2764f9505aa9ed5113
Pedro Brighenti <pedro.brighenti@academiadecodigo.org>
2020-04-28 13:43:08 +0100

    fixed mentions

--- src/enums/messages.js
+++ src/enums/messages.js
@@ -1,8 +1,8 @@
 const ROLES = require('./roles');
 
 const messages = {
-    THIRTY_MIN: `<&${ROLES.EVERYONE}> 30 mins until the next Zoom meeting!`,
-    TEN_MIN: `<&${ROLES.EVERYONE}> 10 mins until the next Zoom meeting!`,
+    THIRTY_MIN: `<@&${ROLES.EVERYONE}> 30 mins until the next Zoom meeting!`,
+    TEN_MIN: `<@&${ROLES.EVERYONE}> 10 mins until the next Zoom meeting!`,
 };
 
 Object.freeze(messages);
